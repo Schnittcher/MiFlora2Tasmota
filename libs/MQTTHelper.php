d3e2ebf4f4641c158e5ddb778b5f7dacef0b0ec0
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-03-31 09:45:25 +0200

    fnmatch für symbox

--- libs/MQTTHelper.php
+++ libs/MQTTHelper.php
@@ -1,6 +1,14 @@
 <?php
 
 declare(strict_types=1);
+
+if (!function_exists('fnmatch')) {
+    function fnmatch($pattern, $string)
+    {
+        return preg_match('#^' . strtr(preg_quote($pattern, '#'), ['\*' => '.*', '\?' => '.']) . '$#i', $string);
+    }
+}
+
 trait TasmotaMQTTHelper
 {
     protected function sendMQTT($Topic, $Payload)
